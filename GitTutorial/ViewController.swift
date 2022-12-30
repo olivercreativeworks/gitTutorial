d360b61a207c4707b19dab3e5b2dc5807a654bd6
Oliver <olivercreativeworks@gmail.com>
2022-12-29 21:31:52 -0800

    Add Click Me button to view controller.

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -9,9 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Click Me", for: .normal)
+        button.backgroundColor = .systemCyan
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemRed
+        
+        self.view.addSubview(self.button)
+        
+        self.button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            button.widthAnchor.constraint(equalToConstant: 120),
+            button.heightAnchor.constraint(equalToConstant: 120)
+        ])
         // Do any additional setup after loading the view.
     }
 
