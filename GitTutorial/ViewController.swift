117f59feca4da66ee7f30461157364878c18ae39
Oliver <olivercreativeworks@gmail.com>
2022-12-29 21:14:06 -0800

    Initial Commit

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by Oliver Allen-Cummings on 12/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemRed
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
