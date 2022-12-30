40bfb5af7e60ea52f86c7394cc4bf0283e151283
Oliver <olivercreativeworks@gmail.com>
2022-12-30 10:42:11 -0800

    Update button functionality to push second view controller onto stack instead of changing background color.

---
+++ GitTutorial/ViewControllers/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by Oliver Allen-Cummings on 12/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    private var button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Click Me", for: .normal)
+        button.backgroundColor = .systemCyan
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemRed
+        self.setup()
+        button.addTarget(self, action: #selector(updateViewOnClick), for: .touchUpInside)
+        // Do any additional setup after loading the view.
+    }
+    
+    private func setup(){
+        self.view.addSubview(self.button)
+
+        self.button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            button.widthAnchor.constraint(equalToConstant: 120),
+            button.heightAnchor.constraint(equalToConstant: 120)
+        ])
+    }
+    
+    @objc private func updateViewOnClick(){
+        let vc = SecondViewController()
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @objc private func changeButtonColorOnClick(){
+        if button.backgroundColor == .systemCyan {
+            button.backgroundColor = .systemGreen
+            view.backgroundColor = .white
+        }
+        else{
+            button.backgroundColor = .systemCyan
+            view.backgroundColor = .black
+        }
+    }
+    
+
+
+}
+
