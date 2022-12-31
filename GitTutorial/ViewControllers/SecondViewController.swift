1fa5dc77dc3c6e228852a1e7aa10d3ca0d6d9d6a
olivercreativeworks <76704559+olivercreativeworks@users.noreply.github.com>
2022-12-31 11:17:28 -0800

    Update to second view (#5)
    Add second screen with a button that leads back to the first screen.
    
    * Add second view controller.
    
    * Update rootViewController to be UINavigationController.
    
    * Update button functionality to push second view controller onto stack instead of changing background color.
    
    * Change background color on second view.
    
    * Move ViewController file into ViewControllers folder.
    
    * Add button to take users from second view back to first view.
    
    * Edit button text.

--- GitTutorial/ViewControllers/SecondViewController.swift
+++ GitTutorial/ViewControllers/SecondViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    
+    private var button:UIButton = {
+        let button = UIButton()
+        button.setTitle("Click Me Too", for: .normal)
+        button.backgroundColor = .systemCyan
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemMint
+        self.setup()
+        button.addTarget(self, action: #selector(transitionToFirstScreenOnClick), for: .touchUpInside)
         // Do any additional setup after loading the view.
     }
+    
+    private func setup(){
+        self.view.addSubview(self.button)
+        self.button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            self.button.widthAnchor.constraint(equalToConstant: 120),
+            self.button.heightAnchor.constraint(equalToConstant: 120)
+        ])
+    }
+    
+    @objc private func transitionToFirstScreenOnClick(){
+        let vc = ViewController()
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+
 
     
     /*
